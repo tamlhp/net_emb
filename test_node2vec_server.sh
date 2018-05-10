0b078ade2e29dfe61d5b29f94392dbad1fa46d5c
ntt <ntt@local>
2018-05-10 21:56:24 +0200

    some pre-test

--- test_node2vec_server.sh
+++ test_node2vec_server.sh
@@ -1,4 +1,5 @@
 source activate python2
+python ./node2vec/src/main.py --input ./node2vec/graph/karate.edgelist --output ./node2vec/emb/karate.emb
 # python ./node2vec/src/main.py --input /dataspace/graphsage/ppi/edgelist/ppi-subgraph.edgelist --output dataspace/graphsage/ppi/emb/ppi-subgraph.emb
-python ./node2vec/src/main.py --input /mnt/storage01/duong/dataspace/graphsage/ppi/edgelist/ppi-G.edgelist --output /mnt/storage01/duong/dataspace/graphsage/ppi/emb/ppi-G.emb
+# python ./node2vec/src/main.py --input /mnt/storage01/duong/dataspace/graphsage/ppi/edgelist/ppi-G.edgelist --output /mnt/storage01/duong/dataspace/graphsage/ppi/emb/ppi-G.emb
 source activate base
