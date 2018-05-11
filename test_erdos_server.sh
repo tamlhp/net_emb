68e5fdfc433d1eb6f8145c559cb70be4b4f491d6
ntt <ntt@local>
2018-05-11 16:34:20 +0200

    test erdos

---
+++ test_erdos_server.sh
@@ -0,0 +1,7 @@
+DATASPACE=/mnt/storage01/duong/dataspace/graph
+n=10000
+p=0.00015
+graph=erdos\,n=${n}\,p=${p}
+source activate python2
+python ./node2vec/src/main.py --input ${DATASPACE}/erdos/edgelist/${graph}.edgelist --output ${DATASPACE/erdos/emb/${graph}.emb
+source activate base
