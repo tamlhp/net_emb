91a67a7d47b6922c39c5a2167c0b8d72c730c690
ntt <ntt@local>
2018-05-12 16:57:17 +0200

    edge2vec

--- test_edge2vec_server.sh
+++ test_edge2vec_server.sh
@@ -1,4 +1,6 @@
 # DATASPACE=/Users/tnguyen/dataspace/graph
 DATASPACE=/mnt/storage01/duong/dataspace/graph
-python ./edge2vec/edge2vec.py --edgelist ${DATASPACE}/wikipedia/edgelist/POS.edgelist --nodeemb ${DATASPACE}/wikipedia/emb/POS.emb \
-    --output ${DATASPACE}/wikipedia/emb/POS-edge.emb --weighted --func avg
+# python ./edge2vec/edge2vec.py --edgelist ${DATASPACE}/wikipedia/edgelist/POS.edgelist --nodeemb ${DATASPACE}/wikipedia/emb/POS.emb \
+    # --output ${DATASPACE}/wikipedia/emb/POS-edge.emb --weighted --func avg
+python ./edge2vec/edge_classifier.py --edgelist ${DATASPACE}/wikipedia/edgelist/POS.edgelist --nodeemb ${DATASPACE}/wikipedia/emb/POS.emb \
+    --output ${DATASPACE}/wikipedia/emb/POS-edge.emb --weighted --func hadamard --verbose
