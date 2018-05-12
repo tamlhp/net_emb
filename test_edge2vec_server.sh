2974cb38ddf47a82d2f24baaebb4c2b8567be1fe
ntt <ntt@local>
2018-05-12 10:09:09 +0200

    edge2vec

--- test_edge2vec_server.sh
+++ test_edge2vec_server.sh
@@ -1,3 +1,4 @@
+# DATASPACE=/Users/tnguyen/dataspace/graph
 DATASPACE=/mnt/storage01/duong/dataspace/graph
-python ./edge2vec/edge2vec.py --edgelist ${DATASPACE}/wikipedia/edgelist/POS.edgelist --nodeemb ${DATASPACE}/wikipedia/emb/POS.emb \,
+python ./edge2vec/edge2vec.py --edgelist ${DATASPACE}/wikipedia/edgelist/POS.edgelist --nodeemb ${DATASPACE}/wikipedia/emb/POS.emb \
     --output ${DATASPACE}/wikipedia/emb/POS-edge.emb --weighted --func avg
