6f1974b3438c33c19d91e021bf0b35f222b6c235
ntt <ntt@local>
2018-05-14 17:10:08 +0200

    edge classifier graphsage

--- test_edge2vec_graphsage_server.sh
+++ test_edge2vec_graphsage_server.sh
@@ -1,5 +1,5 @@
-DATASPACE=/Users/tnguyen/dataspace/graph
-# DATASPACE=/mnt/storage01/duong/dataspace/graph
+# DATASPACE=/Users/tnguyen/dataspace/graph
+DATASPACE=/mnt/storage01/duong/dataspace/graph
 # source activate python2
 python ./edge2vec/edge_classifier_graphsage.py --dataset_dir ${DATASPACE}/wikipedia/graphsage --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/graphsage_mean_small_0.000010 \
     --prefix POS --weighted --func hadamard --verbose
