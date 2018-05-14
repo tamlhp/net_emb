20e41642f256b52df211d8b274311df239444f19
ntt <ntt@local>
2018-05-14 10:09:01 +0200

    graphsage

--- test_graphsage_server.sh
+++ test_graphsage_server.sh
@@ -3,6 +3,6 @@ DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate tensorflow
 cd graphsage/
 # python -m graphsage.utils ${DATASPACE}/wikipedia/graphsage/POS-G.json ${DATASPACE}/wikipedia/graphsage/POS-walks.txt
-python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/wikipedia/graphsage/POS --model graphsage_mean --max_total_steps 1000 --validate_iter 10
+python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/wikipedia/graphsage/POS --model graphsage_mean --max_total_steps 1000 --validate_iter 10 --identity_dim 50
 cd ../
 source activate base
