8d85182072c5e7fb0181f16e686c6cf9ed02ffb9
ntt <ntt@local>
2018-05-16 20:05:49 +0200

    test reddit graphsage

--- test_graphsage_server.sh
+++ test_graphsage_server.sh
@@ -5,7 +5,7 @@ cd graphsage/
 # python -m graphsage.utils ${DATASPACE}/wikipedia/graphsage/POS-G.json ${DATASPACE}/wikipedia/graphsage/POS-walks.txt
 # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/wikipedia/graphsage/POS --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
 #      --identity_dim 64 --base_log_dir ${DATASPACE}/wikipedia/
-python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/wikipedia/graphsage/reddit --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
-     --identity_dim 128 --base_log_dir ${DATASPACE}/wikipedia/
+python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/reddit/graphsage/reddit --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
+     --identity_dim 128 --base_log_dir ${DATASPACE}/reddit/
 cd ../
 source activate base
