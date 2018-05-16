9f178a2ed14b51109533883dc62fb5a28611159f
ntt <ntt@local>
2018-05-16 18:54:53 +0200

    node classification for node2vec

--- node_classification/node_classifier.py
+++ node_classification/node_classifier.py
@@ -1,12 +1,19 @@
 from __future__ import print_function
 import json
+import time
 import numpy as np
 import pdb
+import sys
+sys.path.insert(0, "./")
+from edge2vec import edge2vec
 
+import networkx as nx
 from networkx.readwrite import json_graph
 from argparse import ArgumentParser
 
 def run_regression(train_embeds, train_labels, test_embeds, test_labels, average='micro'):
+    start_time = time.time()
+
     np.random.seed(1)
     from sklearn.linear_model import SGDClassifier
     from sklearn.dummy import DummyClassifier
@@ -15,31 +22,42 @@ def run_regression(train_embeds, train_labels, test_embeds, test_labels, average
     # dummy.fit(train_embeds, train_labels)
     log = SGDClassifier(loss="log", n_jobs=55)
     log.fit(train_embeds, train_labels)
-    print("Test F1-score")
-    print(f1_score(test_labels, log.predict(test_embeds), average=average))
-    print("Train F1-score")
-    print(f1_score(train_labels, log.predict(train_embeds), average=average))
+
+    n2v_scores = {}
+
+    n2v_scores['test_f1'] = f1_score(test_labels, log.predict(test_embeds), average=average)
+    n2v_scores['train_f1'] = f1_score(train_labels, log.predict(train_embeds), average=average)
+    n2v_scores['runtime'] = time.time() - start_time
+
+    print("Test F1-score", n2v_scores['test_f1'])
+    print("Train F1-score", n2v_scores['train_f1'])
+    print("Runtime (s)", n2v_scores['runtime'])
     # print("Random baseline")
     # print(f1_score(test_labels, dummy.predict(test_embeds), average=average))
 
+    return n2v_scores
+
 def parse_args():
     parser = ArgumentParser("Node classification")
+    parser.add_argument("--algorithm", choices=['node2vec', 'graphsage'], default="graphsage", help="network embedding method")
     parser.add_argument("--dataset_dir", default="/Users/tnguyen/dataspace/graph/wikipedia/graphsage", help="Path to directory containing the dataset.")
     parser.add_argument("--embed_dir", default="/Users/tnguyen/dataspace/graph/wikipedia/unsup-graphsage/graphsage_mean_small_0.000010", help="Path to directory containing the learned node embeddings. Set to 'feat' for raw features.")
     parser.add_argument("--prefix", default="POS", help="Prefix to access the dataset")
     parser.add_argument("--setting", choices=['val', 'test'], default="test", help="Either val or test.")
-    parser.add_argument("--f1average", choices=['micro', 'macro', 'weighted', 'none'], default="micro", help="Average strategy for multi-class classification")
+    parser.add_argument("--average", choices=['micro', 'macro', 'weighted', 'none'], default="micro", help="Average strategy for multi-class classification")
     return parser.parse_args()
 
 def main(args):
     dataset_dir = args.dataset_dir
-    data_dir = args.embed_dir
+    emb_dir = args.embed_dir
     setting = args.setting
     prefix = args.prefix
     if args.average == 'none': args.average=None
 
     print("Loading data...")
     G = json_graph.node_link_graph(json.load(open("{0}/{1}-G.json".format(dataset_dir, prefix))))
+    print(nx.info(G))
+
     labels = json.load(open("{0}/{1}-class_map.json".format(dataset_dir, prefix)))
     labels = {int(i):l for i, l in labels.iteritems()}
     
@@ -49,9 +67,9 @@ def main(args):
     if train_labels.ndim == 1:
         train_labels = np.expand_dims(train_labels, 1)
     test_labels = np.array([labels[i] for i in test_ids])
-    print("running", data_dir)
+    print("running", emb_dir)
 
-    if data_dir == "feat":
+    if emb_dir == "feat":
         print("Using only features..")
         feats = np.load("{0}/{1}-feats.npy".format(dataset_dir, prefix))
         ## Logistic gets thrown off by big counts, so log transform num comments and score
@@ -70,11 +88,22 @@ def main(args):
         test_feats = scaler.transform(test_feats)
         run_regression(train_feats, train_labels, test_feats, test_labels, args.average)
     else:
-        embeds = np.load(data_dir + "/val.npy")
-        id_map = {}
-        with open(data_dir + "/val.txt") as fp:
-            for i, line in enumerate(fp):
-                id_map[int(line.strip())] = i
+        if args.algorithm == 'graphsage':
+            embeds = np.load(emb_dir + "/val.npy")
+            id_map = {}
+            with open(emb_dir + "/val.txt") as fp:
+                for i, line in enumerate(fp):
+                    id_map[int(line.strip())] = i
+        elif args.algorithm == 'node2vec':
+            node2vec, num_nodes, dim_size = edge2vec.load_embedding("{0}/{1}.emb".format(emb_dir, prefix))
+            embeds = np.zeros(shape=(num_nodes, dim_size))
+            id_map = {}
+            for i in range(num_nodes):
+                embeds[i] = node2vec[str(i)]
+                id_map[i] = i
+        else:
+            assert False
+
         train_embeds = embeds[[id_map[id] for id in train_ids]] 
         test_embeds = embeds[[id_map[id] for id in test_ids]] 
 
--- test_nodeclassifier_server.sh
+++ test_nodeclassifier_server.sh
@@ -1,8 +1,12 @@
 DATASPACE=/Users/tnguyen/dataspace/graph
 # DATASPACE=/mnt/storage01/duong/dataspace/graph
 # source activate tensorflow
-python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/wikipedia/graphsage/ --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/graphsage_mean_small_0.000010 \
-    --prefix POS --setting test
+# python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
+#     --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/graphsage_mean_small_0.000010 \
+#     --prefix POS --setting test
 # python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/ppi/graphsage/ --embed_dir feat \
     # --prefix ppi --setting test
+python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
+    --embed_dir ${DATASPACE}/wikipedia/emb/ \
+    --prefix POS --setting test
 # source activate base
