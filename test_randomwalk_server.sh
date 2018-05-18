44684cabb6ee0c63f6f91da39a6743516eafba0b
ntt <ntt@local>
2018-05-18 21:39:35 +0200

    test classification

--- test_randomwalk_server.sh
+++ test_randomwalk_server.sh
@@ -2,8 +2,8 @@
 DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate tensorflow
 cd graphsage/
-python -m graphsage.utils ${DATASPACE}/wikipedia/graphsage/POS-G.json ${DATASPACE}/wikipedia/graphsage/POS-walks.txt
-python -m graphsage.utils ${DATASPACE}/reddit/graphsage/reddit-G.json ${DATASPACE}/reddit/graphsage/reddit-walks.txt
-python -m graphsage.utils ${DATASPACE}/ppi/graphsage/ppi-G.json ${DATASPACE}/ppi/graphsage/ppi-walks.txt
+python -m graphsage.utils ${DATASPACE}/wikipedia/graphsage/POS-G.json ${DATASPACE}/wikipedia/POS-walks.txt
+python -m graphsage.utils ${DATASPACE}/reddit/graphsage/reddit-G.json ${DATASPACE}/reddit/reddit-walks.txt
+python -m graphsage.utils ${DATASPACE}/ppi/graphsage/ppi-G.json ${DATASPACE}/ppi/ppi-walks.txt
 cd ../
 source activate base
