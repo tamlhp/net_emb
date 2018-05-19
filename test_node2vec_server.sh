03c657a75a9d762cf039df49c6bd95d80b393e73
ntt <ntt@local>
2018-05-19 16:15:11 +0200

    test node2vec server

--- test_node2vec_server.sh
+++ test_node2vec_server.sh
@@ -1,9 +1,9 @@
 DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate python2
-# python ./node2vec/src/main.py --input ./node2vec/graph/karate.edgelist --output ./node2vec/emb/karate.emb
+python ./node2vec/src/main.py --input ${DATASPACE}/karate/edgelist/karate.edgelist --output ${DATASPACE}/karate/emb/karate.emb
 # python ./node2vec/src/main.py --input /dataspace/graph/ppi/edgelist/ppi-subgraph.edgelist --output dataspace/graph/ppi/emb/ppi-subgraph.emb
 # python ./node2vec/src/main.py --input ${DATASPACE}/ppi/edgelist/ppi-G.edgelist --output ${DATASPACE/ppi/emb/ppi-G.emb
 # python ./node2vec/src/main.py --input ${DATASPACE}/wikipedia/edgelist/POS.edgelist --output ${DATASPACE}/wikipedia/emb/POS.emb --weighted
 # python ./node2vec/src/main.py --input ${DATASPACE}/ppi/edgelist/ppi.edgelist --output ${DATASPACE}/ppi/emb/ppi.emb
-python ./node2vec/src/main.py --input ${DATASPACE}/reddit/edgelist/reddit.edgelist --output ${DATASPACE}/reddit/emb/reddit.emb
+# python ./node2vec/src/main.py --input ${DATASPACE}/reddit/edgelist/reddit.edgelist --output ${DATASPACE}/reddit/emb/reddit.emb
 source activate base
