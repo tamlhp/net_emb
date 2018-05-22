be3f2bf579d66e785ffd46429aaf2f8d70e27e75
ntt <ntt@local>
2018-05-22 16:58:40 +0200

    exp

--- test_graphsage_server.sh
+++ test_graphsage_server.sh
@@ -3,17 +3,18 @@ DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate tensorflow
 cd graphsage/
 
-python -m graphsage.utils ${DATASPACE}/facebook/graphsage/facebook-G.json ${DATASPACE}/facebook/graphsage/facebook-walks.txt
-python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/facebook/graphsage/facebook --model gcn --model_size big \
+# python -m graphsage.utils ${DATASPACE}/facebook/graphsage/facebook-G.json ${DATASPACE}/facebook/graphsage/facebook-walks.txt
+# python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/facebook/graphsage/facebook --model gcn --model_size big \
+#     --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
+#      --identity_dim 128 --base_log_dir ${DATASPACE}/facebook/
+
+python -m graphsage.utils ${DATASPACE}/wikipedia/graphsage/POS-G.json ${DATASPACE}/facebook/graphsage/POS-walks.txt
+python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/wikipedia/graphsage/POS --model gcn --model_size big \
     --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
-     --identity_dim 128 --base_log_dir ${DATASPACE}/facebook/
+     --identity_dim 128 --base_log_dir ${DATASPACE}/wikipedia/
 
 # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/karate/graphsage/karate --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
 #      --identity_dim 128 --base_log_dir ${DATASPACE}/karate/
-
-# python -m graphsage.utils ${DATASPACE}/wikipedia/graphsage/POS-G.json ${DATASPACE}/wikipedia/graphsage/POS-walks.txt
-# python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/wikipedia/graphsage/POS --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
-    #  --identity_dim 128 --base_log_dir ${DATASPACE}/wikipedia/
 
 # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/reddit/graphsage/reddit --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
 #      --identity_dim 128 --base_log_dir ${DATASPACE}/reddit/
