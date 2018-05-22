b309e8184d378f147e10d766d86f2fc553752440
ntt <ntt@local>
2018-05-22 18:20:45 +0200

    exp

--- test_tsv_server.sh
+++ test_tsv_server.sh
@@ -6,5 +6,7 @@ python data/export_tsv.py --algorithm graphsage --prefix facebook --emb ${DATASP
 python data/export_tsv.py --algorithm node2vec --prefix POS --emb ${DATASPACE}/wikipedia/emb/ --tsv ${DATASPACE}/wikipedia/tsv
 python data/export_tsv.py --algorithm graphsage --prefix POS --emb ${DATASPACE}/wikipedia/unsup-graphsage/gcn_big_0.000010/ --tsv ${DATASPACE}/wikipedia/tsv
 
-python data/export_tsv.py --algorithm graphsage --prefix POS --emb ${DATASPACE}/wikipedia/unsup-graphsage/graphsage_mean_small_0.000010/ --tsv ${DATASPACE}/wikipedia/tsv
+python data/export_tsv.py --algorithm node2vec --prefix blog --emb ${DATASPACE}/blogcatalog/emb/ --tsv ${DATASPACE}/blogcatalog/tsv
+python data/export_tsv.py --algorithm graphsage --prefix blog --emb ${DATASPACE}/blogcatalog/unsup-graphsage/gcn_big_0.000010/ --tsv ${DATASPACE}/blogcatalog/tsv
+
 python data/export_tsv.py --algorithm graphsage --prefix ca-astroph --dataset ${DATASPACE}/ca-astroph/unsup-graphsage/graphsage_mean_small_0.000010/ --tsv ${DATASPACE}/ca-astroph/tsv
