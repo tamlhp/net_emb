a407cd0a2ecb00a9677f5187d678844c3ec4e36e
ntt <ntt@local>
2018-05-22 14:11:28 +0200

    exp

--- test_data_server.sh
+++ test_data_server.sh
@@ -2,7 +2,7 @@ DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate tensorflow
 
 python data/convert_to_edgelist_format.py --ppi ${DATASPACE}/ppi --stat
-python data/convert_to_edgelist_format.py --wikipedia ${DATASPACE}/wikipedia --stat
+python data/convert_to_edgelist_format.py --wiki ${DATASPACE}/wikipedia --stat
 python data/convert_to_edgelist_format.py --reddit ${DATASPACE}/reddit --stat
 
 source activate base
