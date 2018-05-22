241a4db02340f50ff8458e0e4963a3a757b1624c
ntt <ntt@local>
2018-05-22 11:08:39 +0200

    exp

--- test_tsv_server.sh
+++ test_tsv_server.sh
@@ -1,6 +1,6 @@
 DATASPACE=/mnt/storage01/duong/dataspace/graph
 
-python data/export_tsv.py --algorithm node2vec --prefix karate --dataset ${DATASPACE}/karate --tsv ${DATASPACE}/karate/tsv
-python data/export_tsv.py --algorithm graphsage --prefix karate --dataset ${DATASPACE}/karate --tsv ${DATASPACE}/karate/tsv
+# python data/export_tsv.py --algorithm node2vec --prefix karate --dataset ${DATASPACE}/karate --tsv ${DATASPACE}/karate/tsv
+# python data/export_tsv.py --algorithm graphsage --prefix karate --dataset ${DATASPACE}/karate --tsv ${DATASPACE}/karate/tsv
 python data/export_tsv.py --algorithm node2vec --prefix POS --dataset ${DATASPACE}/wikipedia --tsv ${DATASPACE}/wikipedia/tsv
 python data/export_tsv.py --algorithm graphsage --prefix POS --dataset ${DATASPACE}/wikipedia --tsv ${DATASPACE}/wikipedia/tsv
