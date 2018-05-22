0d1cc36544999cd75602e8ddaf56d875deb844e4
ntt <ntt@local>
2018-05-22 10:48:45 +0200

    exp

--- graphsage/graphsage/unsupervised_train.py
+++ graphsage/graphsage/unsupervised_train.py
@@ -13,7 +13,7 @@ from graphsage.neigh_samplers import UniformNeighborSampler
 from graphsage.utils import load_data
 
 # os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 # Set random seed
 seed = 123
