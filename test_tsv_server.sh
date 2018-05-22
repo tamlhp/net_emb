948a0bfbce91ca4525d9d2cb6e122152310ee5cd
ntt <ntt@local>
2018-05-22 11:10:54 +0200

    exp

--- test_tsv_server.sh
+++ test_tsv_server.sh
@@ -2,5 +2,7 @@ DATASPACE=/mnt/storage01/duong/dataspace/graph
 
 # python data/export_tsv.py --algorithm node2vec --prefix karate --dataset ${DATASPACE}/karate --tsv ${DATASPACE}/karate/tsv
 # python data/export_tsv.py --algorithm graphsage --prefix karate --dataset ${DATASPACE}/karate --tsv ${DATASPACE}/karate/tsv
-python data/export_tsv.py --algorithm node2vec --prefix POS --dataset ${DATASPACE}/wikipedia --tsv ${DATASPACE}/wikipedia/tsv
-python data/export_tsv.py --algorithm graphsage --prefix POS --dataset ${DATASPACE}/wikipedia --tsv ${DATASPACE}/wikipedia/tsv
+# python data/export_tsv.py --algorithm node2vec --prefix POS --dataset ${DATASPACE}/wikipedia --tsv ${DATASPACE}/wikipedia/tsv
+# python data/export_tsv.py --algorithm graphsage --prefix POS --dataset ${DATASPACE}/wikipedia --tsv ${DATASPACE}/wikipedia/tsv
+python data/export_tsv.py --algorithm node2vec --prefix ca-astroph --dataset ${DATASPACE}/ca-astroph --tsv ${DATASPACE}/ca-astroph/tsv
+python data/export_tsv.py --algorithm graphsage --prefix ca-astroph --dataset ${DATASPACE}/ca-astroph --tsv ${DATASPACE}/ca-astroph/tsv
