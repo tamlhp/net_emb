4b06dcde7c475d029b1a020fde429bf6e9166212
ntt <ntt@local>
2018-05-23 17:30:00 +0200

    exp

--- test_nodeclassifier_server.sh
+++ test_nodeclassifier_server.sh
@@ -10,8 +10,8 @@ source activate tensorflow
 #     --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/gcn_big_0.000010 \
 #     --prefix POS --setting test
 
-# python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
-#     --embed_dir feat --prefix POS --setting test
+python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
+    --embed_dir feat --prefix POS --setting test
 
 # python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/blogcatalog/graphsage/ \
 #     --embed_dir ${DATASPACE}/blogcatalog/emb/ \
@@ -21,22 +21,22 @@ source activate tensorflow
 #     --embed_dir ${DATASPACE}/blogcatalog/unsup-graphsage/gcn_big_0.000010 \
 #     --prefix blog --setting test
 
-# python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/blogcatalog/graphsage/ \
-#     --embed_dir feat --prefix blog --setting test
+python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/blogcatalog/graphsage/ \
+    --embed_dir feat --prefix blog --setting test
 
-python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/ppi/graphsage/ \
-    --embed_dir ${DATASPACE}/ppi/emb/ \
-    --prefix ppi --setting test --label multi --classifier sgd
+# python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/ppi/graphsage/ \
+#     --embed_dir ${DATASPACE}/ppi/emb/ \
+#     --prefix ppi --setting test --label multi --classifier logistic
 
 # python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/ppi/graphsage/ \
 #     --embed_dir ${DATASPACE}/ppi/unsup-graphsage/gcn_big_0.000010 \
-#     --prefix ppi --setting test --label multi --classifier sgd
+#     --prefix ppi --setting test --label multi --classifier logistic
 
-# python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/ppi/graphsage/ \
-#     --embed_dir feat --prefix ppi --setting test --label multi --classifier sgd
+python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/ppi/graphsage/ \
+    --embed_dir feat --prefix ppi --setting test --label multi --classifier logistic
 
-# python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/ppi/graphsage/ --embed_dir feat \
-    # --prefix ppi --setting test
+
+
 
 # python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/reddit/graphsage/ \
 #     --embed_dir ${DATASPACE}/reddit/unsup-graphsage/graphsage_mean_small_0.000010 \
