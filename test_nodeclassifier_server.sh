e23355d76a77e5af057da80507ccbc46b7e5d347
ntt <ntt@local>
2018-05-23 15:43:51 +0200

    exp

--- test_nodeclassifier_server.sh
+++ test_nodeclassifier_server.sh
@@ -2,13 +2,13 @@
 DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate tensorflow
 
-python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
-    --embed_dir ${DATASPACE}/wikipedia/emb/ \
-    --prefix POS --setting test
-
-# python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
-#     --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/gcn_big_0.000010 \
+# python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
+#     --embed_dir ${DATASPACE}/wikipedia/emb/ \
 #     --prefix POS --setting test
+
+python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
+    --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/gcn_big_0.000010 \
+    --prefix POS --setting test
 
 # python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/ppi/graphsage/ --embed_dir feat \
     # --prefix ppi --setting test
