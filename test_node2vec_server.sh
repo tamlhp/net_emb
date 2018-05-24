c3b12fe9f76b99bf51459bc5f963e76cd06c14ec
ntt <ntt@local>
2018-05-24 16:37:43 +0200

    exp

--- test_node2vec_server.sh
+++ test_node2vec_server.sh
@@ -8,8 +8,8 @@ source activate python2
 # python ./node2vec/src/main.py --input ${DATASPACE}/ca-astroph/edgelist/ca-astroph.edgelist --output ${DATASPACE}/ca-astroph/emb/ca-astroph.emb
 # python ./node2vec/src/main.py --input ${DATASPACE}/ppi/edgelist/ppi.edgelist --output ${DATASPACE}/ppi/emb/ppi.emb
 
-# python ./node2vec/src/main.py --input ${DATASPACE}/reddit/edgelist/reddit-int.edgelist --output ${DATASPACE}/reddit/emb/reddit-int.emb
-# python ./data/utils.py --emb ${DATASPACE}/reddit/emb/reddit-int.emb --id ${DATASPACE}/reddit/graphsage/reddit-id_map.json --out ${DATASPACE}/reddit/emb/reddit.emb
+python ./node2vec/src/main.py --input ${DATASPACE}/reddit/edgelist/reddit-int.edgelist --output ${DATASPACE}/reddit/emb/reddit-int.emb
+python ./data/utils.py --emb ${DATASPACE}/reddit/emb/reddit-int.emb --id ${DATASPACE}/reddit/graphsage/reddit-id_map.json --out ${DATASPACE}/reddit/emb/reddit.emb
 
 # IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/erdos/prefix.txt))'
 # for i in "${PREFIX[@]}"
@@ -29,11 +29,11 @@ source activate python2
 #     python ./node2vec/src/main.py --input ${DATASPACE}/watts_graph/edgelist/${i}.edgelist --output ${DATASPACE}/watts_graph/emb/${i}.emb
 # done
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/pa_graph/prefix.txt))'
-for i in "${PREFIX[@]}"
-do
-    python ./node2vec/src/main.py --input ${DATASPACE}/pa_graph/edgelist/${i}.edgelist --output ${DATASPACE}/pa_graph/emb/${i}.emb
-done
+# IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/pa_graph/prefix.txt))'
+# for i in "${PREFIX[@]}"
+# do
+#     python ./node2vec/src/main.py --input ${DATASPACE}/pa_graph/edgelist/${i}.edgelist --output ${DATASPACE}/pa_graph/emb/${i}.emb
+# done
 
 # python ./node2vec/src/main.py --input ${DATASPACE}/karate/edgelist/karate.edgelist --output ${DATASPACE}/karate/emb/karate.emb
 # nice -n 19 python ./node2vec/src/main.py --input ${DATASPACE}/reddit/edgelist/reddit.edgelist --output ${DATASPACE}/reddit/emb/reddit.emb
