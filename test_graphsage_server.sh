49a236287a73b5011e52cfde693eda7427518133
ntt <ntt@local>
2018-05-24 10:59:08 +0200

    exp

--- test_graphsage_server.sh
+++ test_graphsage_server.sh
@@ -29,8 +29,8 @@ cd graphsage/
 #      --identity_dim 128 --base_log_dir ${DATASPACE}/ppi/
 
 # python -m graphsage.utils ${DATASPACE}/reddit/graphsage/reddit-G.json ${DATASPACE}/reddit/graphsage/reddit-walks.txt
-python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/reddit/graphsage/reddit --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
-     --identity_dim 128 --base_log_dir ${DATASPACE}/reddit/
+# python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/reddit/graphsage/reddit --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
+#      --identity_dim 128 --base_log_dir ${DATASPACE}/reddit/
 # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/reddit/graphsage/reddit --model gcn --model_size big \
 #     --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
 #      --identity_dim 128 --base_log_dir ${DATASPACE}/reddit/
@@ -83,6 +83,20 @@ python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/reddit/graphs
 #         --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
 #         --identity_dim 128 --base_log_dir ${DATASPACE}/watts_graph/emb-graphsage/${i}
 # done
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/pa_graph/prefix.txt))'
+for i in "${PREFIX[@]}"
+do
+    python -m graphsage.utils ${DATASPACE}/pa_graph/graphsage/${i}-G.json ${DATASPACE}/pa_graph/graphsage/${i}-walks.txt
+    if [ ! -d "${DATASPACE}/pa_graph/emb-graphsage/${i}" ]; then
+        mkdir ${DATASPACE}/pa_graph/emb-graphsage/${i}
+    fi
+    # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/pa_graph/graphsage/${i} --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
+    #     --identity_dim 128 --base_log_dir ${DATASPACE}/pa_graph/emb-graphsage/${i}
+    python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/pa_graph/graphsage/${i} --model gcn --model_size big \
+        --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
+        --identity_dim 128 --base_log_dir ${DATASPACE}/pa_graph/emb-graphsage/${i}
+done
 
 cd ../
 source activate base
