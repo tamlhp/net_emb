478a944784b75b9186a3633b1899ac0723ff535b
ntt <ntt@local>
2018-05-24 09:43:09 +0200

    exp

--- test_graphsage_server.sh
+++ test_graphsage_server.sh
@@ -40,18 +40,32 @@ cd graphsage/
 #      --identity_dim 128 --base_log_dir ${DATASPACE}/karate/
 
 
+# IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/erdos/prefix.txt))'
+# for i in "${PREFIX[@]}"
+# do
+#     python -m graphsage.utils ${DATASPACE}/erdos/graphsage/${i}-G.json ${DATASPACE}/erdos/graphsage/${i}-walks.txt
+#     if [ ! -d "${DATASPACE}/erdos/emb-graphsage/${i}" ]; then
+#         mkdir ${DATASPACE}/erdos/emb-graphsage/${i}
+#     fi
+#     # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/erdos/graphsage/${i} --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
+#     #     --identity_dim 128 --base_log_dir ${DATASPACE}/erdos/emb-graphsage/${i}
+#     python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/erdos/graphsage/${i} --model gcn --model_size big \
+#         --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
+#         --identity_dim 128 --base_log_dir ${DATASPACE}/erdos/emb-graphsage/${i}
+# done
+
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/erdos/prefix.txt))'
 for i in "${PREFIX[@]}"
 do
-    python -m graphsage.utils ${DATASPACE}/erdos/graphsage/${i}-G.json ${DATASPACE}/erdos/graphsage/${i}-walks.txt
-    if [ ! -d "${DATASPACE}/erdos/emb-graphsage/${i}" ]; then
-        mkdir ${DATASPACE}/erdos/emb-graphsage/${i}
+    python -m graphsage.utils ${DATASPACE}/rr_graph/graphsage/${i}-G.json ${DATASPACE}/rr_graph/graphsage/${i}-walks.txt
+    if [ ! -d "${DATASPACE}/rr_graph/emb-graphsage/${i}" ]; then
+        mkdir ${DATASPACE}/rr_graph/emb-graphsage/${i}
     fi
-    # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/erdos/graphsage/${i} --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
-    #     --identity_dim 128 --base_log_dir ${DATASPACE}/erdos/emb-graphsage/${i}
-    python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/erdos/graphsage/${i} --model gcn --model_size big \
+    # python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/rr_graph/graphsage/${i} --model graphsage_mean --max_total_steps 1000 --validate_iter 10 \
+    #     --identity_dim 128 --base_log_dir ${DATASPACE}/rr_graph/emb-graphsage/${i}
+    python -m graphsage.unsupervised_train --train_prefix ${DATASPACE}/rr_graph/graphsage/${i} --model gcn --model_size big \
         --epochs 10 --dropout 0.01 --weight_decay 0.01 --max_total_steps 100000 --validate_iter 1000 \
-        --identity_dim 128 --base_log_dir ${DATASPACE}/erdos/emb-graphsage/${i}
+        --identity_dim 128 --base_log_dir ${DATASPACE}/rr_graph/emb-graphsage/${i}
 done
 
 cd ../
