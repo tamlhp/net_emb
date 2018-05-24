65b74c16e09fb3715070c10156df4d3d05a006c5
ntt <ntt@local>
2018-05-24 18:00:55 +0200

    exp

--- test_nodeclassifier_server.sh
+++ test_nodeclassifier_server.sh
@@ -2,9 +2,9 @@
 DATASPACE=/mnt/storage01/duong/dataspace/graph
 source activate tensorflow
 
-# python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
-#     --embed_dir ${DATASPACE}/wikipedia/emb/ \
-#     --prefix POS --setting test
+python node_classification/node_classifier.py --algorithm node2vec --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
+    --embed_dir ${DATASPACE}/wikipedia/emb/ \
+    --prefix POS --setting test
 
 # python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/wikipedia/graphsage/ \
 #     --embed_dir ${DATASPACE}/wikipedia/unsup-graphsage/gcn_big_0.000010 \
@@ -29,9 +29,9 @@ source activate tensorflow
 # python node_classification/node_classifier.py --dataset_dir ${DATASPACE}/ppi/graphsage/ \
 #     --embed_dir feat --prefix ppi --setting test --label multi --classifier logistic
 
-python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/reddit/graphsage/ \
-    --embed_dir ${DATASPACE}/reddit/unsup-graphsage/graphsage_mean_small_0.000010 \
-    --prefix reddit --setting test
+# python node_classification/node_classifier.py --algorithm graphsage --dataset_dir ${DATASPACE}/reddit/graphsage/ \
+#     --embed_dir ${DATASPACE}/reddit/unsup-graphsage/graphsage_mean_small_0.000010 \
+#     --prefix reddit --setting test
 
     
 source activate base
