1f136ceea2079e600ea945528b5a76d3d4b43b12
ntt <ntt@local>
2018-05-27 17:06:39 +0200

    slides

---
+++ data/convert_to_gexf_format.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, division
+import argparse
+import networkx as nx
+
+def main(args):
+    G = nx.read_edgelist(args.edgelist)
+    nx.write_gexf(G, args.gexf)
+
+    print(nx.info(G))
+    if args.stat:
+        # print("Diameter: " + str(nx.diameter(G)))
+        print("Avg. clustering coefficient: " + str(nx.average_clustering(G)))
+        print("# Triangles: " + str(sum(nx.triangles(G).values()) / 3))
+    return
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Convert graph data to gexf format.")
+    parser.add_argument('--edgelist', nargs='?', default='', help='Input data path')
+    parser.add_argument('--gexf', nargs='?', default='', help='Output data path')
+    parser.add_argument('--stat', action='store_true', default=False, help='Some statistics')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ test_gexf_server.sh
@@ -0,0 +1,20 @@
+DATASPACE=/mnt/storage01/duong/dataspace/graph
+
+python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/facebook/edgelist/facebook.edgelist --gexf ${DATASPACE}/facebook/gexf/facebook.gexf
+python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/wikipedia/edgelist/POS.edgelist --gexf ${DATASPACE}/wikipedia/gexf/POS.gexf
+python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/blogcatalog/edgelist/blog.edgelist --gexf ${DATASPACE}/blogcatalog/gexf/blog.gexf
+python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/ca-astroph/edgelist/ca-astroph.edgelist --gexf ${DATASPACE}/ca-astroph/gexf/ca-astroph.gexf
+python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/ppi/edgelist/ppi.edgelist --gexf ${DATASPACE}/ppi/gexf/ppi.gexf
+python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/reddit/edgelist/reddit.edgelist --gexf ${DATASPACE}/reddit/gexf/reddit.gexf
+
+# IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/watts_graph/prefix.txt))'
+# for i in "${PREFIX[@]}"
+# do
+#     python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/watts_graph/edgelist/${i}.edgelist --gexf ${DATASPACE}/watts_graph/gexf/${i}.gexf
+# done
+
+# IFS=$'\r\n' GLOBIGNORE='*' command eval  'PREFIX=($(cat ${DATASPACE}/pa_graph/prefix.txt))'
+# for i in "${PREFIX[@]}"
+# do
+#     python data/convert_to_gexf_format.py --edgelist ${DATASPACE}/pa_graph/edgelist/${i}.edgelist --gexf ${DATASPACE}/pa_graph/gexf/${i}.gexf
+# done
